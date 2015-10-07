eded8489ffc0723b3decde38372d3fb2191cf3c1
Francisco Rangel <frankjrangel@gmail.com>
2015-10-07 11:56:31 -0400

    Segundo corte

--- front-page.php
+++ front-page.php
@@ -1,6 +1,96 @@
 <?php get_header(); ?>
+<div id="front-welcome">
+</div>
+<div id="nosotros" class="paneles">
+  
+  <?php // ES 
 
-<?php if ( get_field( 'nosotros' ) ) { the_field( 'nosotros' ); } ?>
-<?php if ( get_field( 'about_us' ) ) { the_field( 'about_us' ); } ?>
+  if ( get_field( 'nosotros' ) ) :
 
+    $nosotros = get_field_object('nosotros');
+    $titulo_nosotros = $nosotros['label'];
+    $texto_nosotros = $nosotros['value'];
+
+  //EN
+    
+  elseif ( get_field( 'about_us' ) ) :
+
+    $nosotros = get_field_object('about_us');
+    $titulo_nosotros = $nosotros['label'];
+    $texto_nosotros = $nosotros['value'];
+  
+  endif; ?>
+
+  <h1 class="titulo-seccion"><?php echo $titulo_nosotros; ?></h1>
+  <div class="texto-seccion"><?php echo $texto_nosotros; ?></div>
+
+</div>
+<span class="separador"></span>
+<div id="que-hacemos" class="paneles">
+  <h2 id="titulo-que-hacemos" class="subtitulo-seccion">
+    <?php //ES 
+
+    if ( get_field( 'que-hacemos' ) ) :
+
+      $que_hacemos = get_field_object('que-hacemos');
+      $titulo_que_hacemos = $que_hacemos['label'];
+      $texto_que_hacemos = $que_hacemos['value'];
+  
+    elseif ( get_field( 'what-we-do' ) ) :
+
+      $nosotros = get_field_object('about_us');
+      $titulo_nosotros = $nosotros['label'];
+      $texto_nosotros = $nosotros['value'];
+       
+    endif; ?> 
+    <?php echo $titulo_que_hacemos; ?>
+  </h2>
+  <div id="contenido-que-hacemos" class="texto-seccion">
+    <div class="filtro"> 
+      <span class="flecha"></span>
+      <?php echo $texto_que_hacemos; ?>
+    </div>
+  </div>
+  <span class="separador"></span>
+<div>
+<div id="nuestros-valores" class="paneles">
+  <h2 id="titulo-nuestros-valores" class="subtitulo-seccion">
+    <?php
+    
+    if ( pll_current_language() == 'en' )
+      $titulo = 'OUR VALUES';
+    else 
+      $titulo = 'NUESTROS VALORES';
+    
+    echo $titulo;
+    ?>  
+  </h2>
+  <div id="contenido-nuestros-valores">
+    <?php
+    
+    $query = new WP_Query( array( 'post_type' => 'valores' ) );  
+
+    if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();  
+
+    ?>
+    <div class="valores-item">
+      <?php if ( get_field( 'icono-valores' ) ) { the_field( 'icono-valores' ); } ?> 
+      <h3><?php the_title(); ?></h3>
+      <?php the_content(); ?>
+    </div>
+    <?php endwhile; endif; ?>
+  </div>
+</div>
+<div id="equipo" class="paneles">
+
+</div>
+<div id="servicios" class="paneles">
+
+</div>
+<div id="noticias" class="paneles">
+
+</div>
+<div id="ubicanos" class="paneles">
+
+</div>
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+function remove_menus() {
+  remove_menu_page( 'edit-comments.php' );
+}
+add_action( 'admin_menu', 'remove_menus' );
 
 function register_menu_principal() {
   register_nav_menus(
@@ -10,6 +14,53 @@ function register_menu_principal() {
 }
 add_action( 'init', 'register_menu_principal' );
 
+function tm_valores() {
+  $labels = array(
+    'name' => 'Valores',
+    'singular_name' => 'Valor',
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'has_archive' => true,
+  );
+
+  register_post_type( 'valores', $args );
+}
+add_action( 'init', 'tm_valores' );
+
+function tm_equipo() {
+  $labels = array(
+    'name' => 'Equipo',
+    'singular_name' => 'Miembro',
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'has_archive' => true,
+  );
+
+  register_post_type( 'equipo', $args );
+}
+add_action( 'init', 'tm_equipo' );
+
+function tm_productos_servicios() {
+  $labels = array(
+    'name' => 'Productos y Servicios',
+    'singular_name' => 'Producto/Servicio',
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'has_archive' => true,
+  );
+
+  register_post_type( 'productosyservicios', $args );
+}
+add_action( 'init', 'tm_productos_servicios' );
 
 
 ?>
