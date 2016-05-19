711496b7688509eb9a7f8a527c4c98006bc44dca
Tony Rodriguez Martinez <tony.a.rodriguez@live.com>
2016-05-18 21:52:07 -0400

    Multiples cambios avanzando el e-book page (no incluye imagenes por alguna razon)

--- page-ebook.php
+++ page-ebook.php
@@ -4,94 +4,118 @@
 </div>
 <div id="noticias" class="paneles ebook">
 
-    <div class="descripcion-ebook">
+    <div class="titulo">
         <h1>Sales Compensation Ebook</h1>
-
-        <?php if ( have_posts() ) :
-            while ( have_posts() ) : the_post() ?>
-            <div>
-                <div class="portada">
-                    <?php the_post_thumbnail('small'); ?>
-                    <?php if ( pll_current_language() == 'en' ) : ?>
-                        <button class="boton-descarga">Download Intro</button>
-                    <?php else: ?>
-                        <button class="boton-descarga">Descargar Introducción</button>
-                    <?php endif; ?>
-                </div>
-            <div class="formulario">
-
-                <div id="errors"></div>
-
-                <?php if ( pll_current_language() == 'en' ) : ?>
-                    <h3>Fill out this form to download the introductory chapter</h3>
-                    <form action="<?php echo get_template_directory_uri(); ?>/process_formulario_ebook.php" method="POST">
-                        <div id="email-group" class="mc-field-group">
-                            <label for="email">Email</label>
-                            <input type="text" name="email" data-parsley-required data-parsley-type="email">
-                        </div>
-                        <div id="name-group" class="mc-field-group">
-                            <label for="fname" id="nombre">First Name</label>
-                            <input type="text" name="fname">
-                        </div>
-                        <div id="lname-group" class="mc-field-group">
-                            <label for="lname">Last Name</label>
-                            <input type="text" name="lname">
-                        </div>
-                        <div id="company-group" class="mc-field-group">
-                            <label for="company">Company</label>
-                            <input type="text" name="company">
-                        </div>
-                        <div id="position-group" class="mc-field-group">
-                            <label for="position">Position</label>
-                            <input type="text" name="position">
-                        </div>
-                        <div id="checkbox-group" class="mc-field-group">
-                            <input type="checkbox" name="subscribe" style="width:30px;" id="subscribe">
-                            <label for="subscribe" style="width:70%">Subscribe to our newsletter</label>
-                        </div>
-                        <input type="submit" class="button" value="SUBMIT" />
-                    </form>
-                    <div style="clear:both;width:100%;"></div>
-
-                <?php else: ?>
-
-                    <h3>Llena este formulario para descargar el capitulo introductorio</h3>
-                    <form action="<?php echo get_template_directory_uri(); ?>/process_formulario_ebook.php" method="POST">
-                        <div id="email-group" class="mc-field-group">
-                            <label for="email">Email</label>
-                            <input type="text" name="email" data-parsley-required data-parsley-type="email">
-                        </div>
-                        <div id="name-group" class="mc-field-group">
-                            <label for="fname" id="nombre">Nombre</label>
-                            <input type="text" name="fname">
-                        </div>
-                        <div id="lname-group" class="mc-field-group">
-                            <label for="lname">Apellido</label>
-                            <input type="text" name="lname">
-                        </div>
-                        <div id="company-group" class="mc-field-group">
-                            <label for="company">Empresa</label>
-                            <input type="text" name="company">
-                        </div>
-                        <div id="position-group" class="mc-field-group">
-                            <label for="position">Cargo</label>
-                            <input type="text" name="position">
-                        </div>
-                        <div id="checkbox-group" class="mc-field-group">
-                            <input type="checkbox" name="subscribe" style="width:30px;" id="subscribe">
-                            <label for="subscribe" style="width:70%">Suscríbete a nuestro newsletter</label>
-                        </div>
-                        <input type="submit" class="button" value="ENVIAR" />
-                    </form>
-                    <div style="clear:both;width:100%;"></div>
-
-                <?php endif; ?>
-            </div><!-- end #formulario -->
-        </div>
-        <div id="texto-descripcion">
-            <?php the_content(); ?>
-        </div>
-        <?php endwhile; endif; ?>
+        <h3>A thoretical and practical methodology for designing and implementing Sales 
+        Incentive Plans for the Sales Force</h3>
+    </div>
+    <div class="share">
+        <h3>Share</h3>
+    </div>
+    <div class="libro">
+    </div>
+    <div class="download">
+        <h3>Download</h3>
+    </div>
+    <div class="descripcion-ebook">
+        <h5>Sales Compensation is a systematic method for designing sales incenvite plans 
+        suitable for any level of expertise. This book offers complete, precise and empirical 
+        qualities, structure and possible outcomes in relation to Designing Sales Compensation 
+        Plans, adapted to specific needs and characteristics of the business, market and 
+        sales force requirements. Starting aligning Sales strategies and Incenvite plan and 
+        ending with the implementation of the plan, the writer guides you throughout the 
+        design of a healthy and well balanced compensation plan to achieve the best benefits 
+        both for the sales force and the business results. Sales Compensation is the product 
+        of a profound knowledge and Lopez' 25-year experience in this field.</h5>
+        <h1>The book covers aspects such as:</h1>
+        <p>*The essential elements to consider to develop and incentive plan for sales 
+        force ("Sales Compensation DNA")</p>
+        <p>*"Sales COmpensation Doctor": a guide to resolve cases</p>
+        <p>*More than 15 examples applied to specific situation throughout the book</p>
+        <p>*A chapter of practical application with solved cases</p>
+        <p>*Rational aspects of each decision contructing sales incentive plans</p>
+        <p>*How to measure the intangible aspects of the Total Rewards' sales force</p>
+    </div>
+    <div id="amazon">
+        <div class="amazon">Texto prueba amazon</div>
+    </div>
+    <div id="amazon">
+        <div class="amazon">Texto prueba tmc</div>
+    </div>
+    <div class="descripcion-ebook">
+        <h3>Reviews</h3>
+        <br />
+        <p>
+            &lt;&lt;This is a book that sales compensation plan designers will certainly use
+        over and over. While the cases and examples reflect practices in Latin America, the
+        concepts, principles and processes it provides transcends geographies and cultures.
+        It is a valuable contribution to the sales compensation body of knowledge&gt;&gt;
+        </p>
+        <br />
+        <h5>Jerome A. Colletti</h5>
+        <h6>Managing Partner *Colletti-Fiss, LLC.</h6>
+        <h6>Scottsdale, AZ, USA</h6>
+        <br />
+        <p>
+            &lt;&lt;A robust and continous revenue growth is paramount to the success and
+        financial valuation of an enterprise. A well motivated and compensated sales force is
+        an important step towards this objective. Federico Lopez' book, Sales Compensation
+        presents detailed information on several and different methods to compensate sales
+        forces that any CEO or entrepeneur should carefully study&gt;&gt;
+        </p>
+        <br />
+        <h5>Gustavo Roosen</h5>
+        <h6>President *IESA</h6>
+        <h6>Caracas</h6>
+        <br />
+        <p>
+            &lt;&lt;Mr. Lopez' book provides a context and framework for the field of sales
+        compensation that few others have been able to do so clearly. His philosophy to
+        design, implement and measure incentive plans shines a new light on this topic and
+        should be reviewed by any enterprise compensation team looking to make an
+        impact&gt;&gt;
+        </p>
+        <br />
+        <h5>Veronica Ferrante</h5>
+        <h6>MBA Compensation Manager</h6>
+    </div>
+    <div class="descripcion-ebook">
+        <h3>Contact the author</h3>
+        <p>Federico Lopez Federico.lopez@thomasmorecg.net</p>
+        <br />
+        <p>With 25 years' experience and over 100 clients served both at home and abroad, 
+        Federico Lopez received his Master's defrees in Statistics and an Expansion in 
+        Mathematical Economics and computerized Statistical Methods at the Central University 
+        of Venezuela (UCV) and a Master's Defreee in Economic Policy at Andres Bello 
+        Catholic University (UCAB) in Caracas, where he graduated in Industrial Relations. 
+        His experience includes the majority of functional areas in different multinational 
+        and national organization such as Xerox, IBM and Caterpillar; corporations where he 
+        occupied positins in the areas of Human Resources, Logistics, External Relations, 
+        Economic Consulting and Finances. He was Assistant General Manager-President of IBM 
+        Andina, Director of Post-Sales and Logistics, Sales Director, General Manager and 
+        Director of Performance & rewards Practice of William M. Mercer de Venezuela. He 
+        was the founder and General Director of ESC Corporation (1992-2000), a pioneer 
+        enterprise of online salary surveys with which he developed the First Advanced Program 
+        in Compensation in Venezuela, in the year 1998</p>
+        <p>He is the founder and Managing Partner, since 2003 of Thomas More Consulting Group; 
+        a management consulting firm with an emphasis on compensation and benefits, with which 
+        he has implemented and developed local, regional and global projects with national 
+        and multinational corporations, insurance and baking sector leaders, hi-tech, 
+        Laboratories, Massive Consumptiond and Retail in Venezuela and in Colombia, Brazil, 
+        Panama, Singapore, Dominican Republic, Chile and Spain among others. </p>
+        <p>For over 30 years, he was a professor at the Social Sciences School of the UCAB 
+        teaching Statistics and since 1995, Costis in compensation and Incenvie plans 
+        and variable complensation, in compensation and benefits. He is a professor of IESA in 
+        compensation, Total Rewards and sales compensation. He has been a professor in evants 
+        and talks of different chambers, associations and institutions such as VenAmCham, 
+        Asociacion Bancaria de Venezuela -Venezuelan Banking Association-, Camara Aseguradora 
+        de Venezuela -Venezuelan Insurance Chamber-, IESA, UCV and UCAB in Venezuela and 
+        abroad, in countries as Ireland, Colombia, Mexico, Brazil, Singapore, Panama, 
+        Dominican Republic and Spain at the IE Business School (Madrid) where he has been 
+        an invited professor. He was a member of Actuarial Science International Associates 
+        (Asinta) Global Executive Committee. His articles have been released by different 
+        publichers and institutions, among them WorldatWork Journal of WorldatWork 
+        (formerly, Amercian Compensation Association).</p>
     </div>
 <div style="width:100%;clear:both;"></div>
 </div>
