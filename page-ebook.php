0cefda59e4064524d70ae20329196ed413540c42
Francisco Rangel <frankjrangel@gmail.com>
2016-05-19 00:58:00 -0430

    80% version desktop

--- page-ebook.php
+++ page-ebook.php
@@ -1,58 +1,76 @@
 <?php get_header(); ?>
+<!-- <meta http-equiv="refresh" content="3"> -->
 <div id="front-ebook">
 
 </div>
 <div id="noticias" class="paneles ebook">
 
     <div class="descripcion-ebook">
-        <h1>Sales Compensation</h1>
-        <h3>
-            A theoretical and practical methodology for designing
-            and implementing Sales Incentive Plans for the Sales Force
-        </h3>
-        <div id="portada">
-
+        <div class="titulo">
+            <h1>Sales Compensation</h1>
+            <h3>
+                A theoretical and practical methodology for designing
+                and implementing Sales Incentive Plans for the Sales Force
+            </h3>
+        </div>
+        <div class="wrapper-portada">
+            <div id="portada"></div>
+            <button id="descarga">
+                DOWNLOAD
+                <span>introduction</span>
+            </button>
         </div>
         <div id="texto-descripcion">
-            Sales Compensation is a systematic method for designing sales incentive
-            plans suitable for any level of expertise. This book offers complete, precise
-            and empirical qualities, structure and possible outcomes in relation to
-            Designing Sales Compensation Plans, adapted to specific needs and
-            characteristics of the business, market and sales force requirements.
-            Starting aligning Sales strategies and Incentive plan and ending with the
-            implementation of the plan, the writer guides you throughout the design of
-            a healthy and well balanced compensation plan to achieve the best benefits
-            both for the sales force and the business  results. Sales Compensation is the
-            product of a profound knowledge and López’ 25-year- experience in this
-            field
+            <p>
+                Sales Compensation is a systematic method for designing sales incentive
+                plans suitable for any level of expertise. This book offers complete, precise
+                and empirical qualities, structure and possible outcomes in relation to
+                Designing Sales Compensation Plans, adapted to specific needs and
+                characteristics of the business, market and sales force requirements.
+                Starting with aligning Sales strategies and Incentive plan and ending with the
+                implementation of the plan, the writer guides you throughout the design of
+                a healthy and well balanced compensation plan to achieve the best benefits
+                both for the sales force and the business  results. Sales Compensation is the
+                product of a profound knowledge and López’ 25-year- experience in this
+                field
+            </p>
+            <div class="list">
+                <h2>The book covers aspects such as:</h2>
+                <ul>
+                    <li>
+                        The essential elements to consider to develop an incentive plan for sales
+                        force (“Sales Compensation DNA”)
+                    </li>
+                    <li>"Sales Compensation Doctor": a guide to resolve cases</li>
+                    <li>More than 15 examples applied to specific situations throughout the book</li>
+                    <li>A chapter of  practical application with solved cases</li>
+                    <li>Rational aspects of each decision constructing sales incentive plans</li>
+                    <li>How to measure the intangible aspects of the Total Rewards´ sales force</li>
+                </ul>
+            </div>
+            <div class="reach">
+                <a href="#">
+                    <div class="amazon"></div>
+                </a>
+                <a href="<?php echo home_url(); ?>" target="_blank">
 
-            <h2>The book covers aspects such as:</h2>
-
-            <ul>
-                <li>
-                    The essential elements to consider to develop an incentive plan for sales
-                    force (“Sales Compensation DNA”)
-                </li>
-                <li>"Sales Compensation Doctor": a guide to resolve cases</li>
-                <li>More than 15 examples applied to specific situations throughout the book</li>
-                <li>A chapter of  practical application with solved cases</li>
-                <li>Rational aspects of each decision constructing sales incentive plans</li>
-                <li>How to measure the intangible aspects of the Total Rewards´ sales force</li>
-            </ul>
+                    <div class="tmc"></div>
+                </a>
+            </div>
         </div>
     </div>
 
     <div class="reviews">
-        <div class="left-fill"></div>
-        <div class="wrapper-review">
+        <div class="linea-verde"></div>
+        <div class="left-fill reviews-fill"></div>
+        <div class="wrapper-reviews">
+            <h2>Reviews</h2>
             <div class="review">
                 <p>
-                &laquo;
-                This is a book that sales compensation plan designers will certainly use over and over.
+                &laquo;This is a book that sales compensation plan designers will certainly use over and over.
                 While the cases and examples reflect practices in Latin America, the concepts,
                 principles and processes it provides transcends geographies and cultures.
-                It is a valuable contribution to the sales compensation body of knowledge
-                &raquo;
+                It is a valuable contribution to the sales compensation body of knowledge&raquo;
                 </p>
                 <p class="author">Jerome A. Colleti,</p>
                 <p class="position">
@@ -62,13 +80,11 @@
             </div>
             <div class="review">
                 <p>
-                &laquo;
-                A robust and continuous revenue growth is paramount to the success and
+                &laquo;A robust and continuous revenue growth is paramount to the success and
                 nancial valuation of an enterprise. A well motivated and compensated sales
                 force is an important step towards this objective. Federico López's book, Sales
                 Compensation presents detailed information on several and dierent methods
-                to compensate sales forces that any CEO or entrepreneur should carefully study
-                &raquo;
+                to compensate sales forces that any CEO or entrepreneur should carefully study&raquo;
                 </p>
                 <p class="author">Gustavo Roosen</p>
                 <p class="position">
@@ -77,22 +93,68 @@
             </div>
             <div class="review">
                 <p>
-                &laquo;
-                Mr. López’s book provides a context and framework for the eld of sales
+                &laquo;Mr. López’s book provides a context and framework for the eld of sales
                 compensation that few others have been able to do so clearly. His philosophy
                 to design, implement and measure incentive plans shines a new light on this
                 topic and should be reviewed by any enterprise compensation team looking to
-                make an impact
-                &raquo;
+                make an impact&raquo;
                 </p>
                 <p class="author">Veronica Ferrante</p>
                 <p class="position">
                     MBA. Compensation Manager.
                 </p>
             </div>
-        </div><!-- end wrapper-review -->
-    </div><!-- end reviews -->
+        </div><!-- end .wrapper-review -->
+    </div><!-- end .reviews -->
 
+    <div id="author">
+        <div class="left-fill author-fill">
+            <div id="author-pic"></div>
+        </div>
+        <div class="wrapper-author-info">
+            <h2>Contact the author</h2>
+            <p>
+                Federico Lopez
+                <a href="mailto:federico.lopez@thomasmorecg.net">
+                    federico.lopez@thomasmorecg.net
+                </a>
+            <p>
+                With 25 years’ experience and over 100 clients served both at home and
+                abroad, Federico López received his Master’s Degrees in Statistics and an
+                Expansion in Mathematical Economics and Computerized Statistical
+                Methods at the Central University of Venezuela (UCV), and a Master’s
+                Degree in Economic Policy at Andrés Bello Catholic University (UCAB)
+                in Caracas, where he graduated in Industrial Relations.
+            </p>
+            <p>
+                His experience includes the majority of functional areas in different
+                multinational and national organizations such as Xerox, IBM, and
+                Caterpillar, corporations where he occupied positions in the areas of
+                Human Resources, Logistics, External Relations, Economic Consulting,
+                Sales and Finance.
+            </p>
+            <p>
+                He is the founder and Managing Partner, since 2003, of Thomas More
+                Consulting Group, a management consulting firm with an emphasis on
+                compensation and benefits, with which he has implemented and developed
+                local, regional and global projects with national and multinational
+                corporations.
+            </p>
+            <p>
+                For over 30 years, he was a professor at the Social Sciences School of the
+                UCAB teaching Statistics, and since 1995, Costs in compensation and
+                Incentive plans and variable compensation, in compensation and benefits.
+                He is a professor of IESA in compensation, Total Rewards and sales
+                compensation. He has been a Professor in events and talks of different
+                chambers, associations and institutions in Venezuela Ireland, Colombia,
+                Mexico, Brazil, Singapore, Panama and Dominican Republic and Spain at
+                the IE Business School (Madrid) where has been invited professor. His
+                articles have been released by different publishers and institutions, among
+                them, WorldatWork Journal of WorldatWork (formerly, American
+                Compensation Association).
+            </p>
+        </div>
+    </div> <!-- end #author -->
 
 <div style="width:100%;clear:both;"></div>
 </div>
